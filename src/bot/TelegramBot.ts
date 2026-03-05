132dba88b61574b2e4d5a1f726c19e667305abbb
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-03-05 12:46:23 -0300

    chore: create TelegramBot Class

---
+++ src/bot/TelegramBot.ts
@@ -0,0 +1,10 @@
+import { Bot } from "grammy"
+
+export class TelegramBot{
+    constructor(private readonly bot: Bot){}
+
+    start(): void {
+        console.log("PromoWatch is On 🚀")
+        this.bot.start()
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Bot } from "grammy";
+import { TelegramBot } from "./bot/TelegramBot.js";
+import { env } from "./config/Env.js";
+
+const bot = new TelegramBot(new Bot(env.TELEGRAM_TOKEN))
+
+bot.start()
