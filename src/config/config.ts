e434326c489f4040657508f2beaf3a5252e80a5e
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-04-30 16:15:19 -0300

    config: update environment variables in config.ts and add types to tsconfig.json

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,9 @@ import "dotenv/config"
 export const env = {
     TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN!,
     DATABASE_URL: process.env.DATABASE_URL!,
-    AUTHORIZED: process.env.AUTHORIZED!
+    AUTHORIZED: process.env.AUTHORIZED!,
+    ML_ID: process.env.ML_ID!,
+    ML_SECRET: process.env.ML_SECRET,
+    ML_URI: process.env.ML_URI
+
 }
