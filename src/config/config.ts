34d0c22342593c608efbfeca9517ace99470f8e0
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-04-30 18:32:45 -0300

    style: reorder environment variables for improved organization in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,12 @@
 import "dotenv/config"
 
 export const env = {
-    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN!,
-    DATABASE_URL: process.env.DATABASE_URL!,
-    AUTHORIZED: process.env.AUTHORIZED!,
-    ML_ID: process.env.ML_ID!,
-    ML_SECRET: process.env.ML_SECRET,
-    ML_URI: process.env.ML_URI
+  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN!,
+  AUTHORIZED: process.env.AUTHORIZED!,
+
+  DATABASE_URL: process.env.DATABASE_URL!,
+
+  ML_ID: process.env.ML_ID!,
+  ML_SECRET: process.env.ML_SECRET,
+  ML_URI: process.env.ML_URI,
 }
